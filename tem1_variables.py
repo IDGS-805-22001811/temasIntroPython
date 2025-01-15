27838ef4f815bf96e2c4675d3497e792e0400fac
Josue Chavez <79125@alumnos.utleon.edu.mx>
2025-01-14 19:15:45 -0600

    Primer commit

---
+++ tem1_variables.py
@@ -0,0 +1,13 @@
+# variables int, float string, no existe en el tipado por defecto
+
+num1=2
+num2=3.5
+nombre="Josue"
+print(num1)
+print(num2)
+print(nombre)
+
+n1="7"
+n2="8"
+suma=int(n1)+int(n2)
+print("La suma es: ",suma)
